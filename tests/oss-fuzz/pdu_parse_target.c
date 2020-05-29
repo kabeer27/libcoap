d1af5332085f77a788799e93a05f47e75e666566
kabeer27 <kabeer.205@gmail.com>
2020-05-29 10:39:06 +0530

    Fixing small error

--- tests/oss-fuzz/pdu_parse_target.c
+++ tests/oss-fuzz/pdu_parse_target.c
@@ -2,17 +2,20 @@
 
 int
 LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-    
     const uint8_t *data_tcp = data;
-    if (pdu) {
+    
+    coap_pdu_t *pdu_udp = coap_pdu_init(0, 0, 0, size);
+    if (pdu_udp) {
         coap_set_log_level(LOG_DEBUG);
-        coap_pdu_t *pdu_udp = coap_pdu_init(0, 0, 0, size);
         coap_pdu_parse(COAP_PROTO_UDP, data, size, pdu_udp);
         coap_pdu_encode_header(pdu_udp, COAP_PROTO_UDP);
         coap_show_pdu(LOG_DEBUG, pdu_udp);
         coap_delete_pdu(pdu_udp);
+    }
 
-        coap_pdu_t *pdu_tcp = coap_pdu_init(0, 0, 0, size);
+    coap_pdu_t *pdu_tcp = coap_pdu_init(0, 0, 0, size);
+    if (pdu_tcp) {
+        coap_set_log_level(LOG_DEBUG);
         coap_pdu_parse(COAP_PROTO_TCP, data_tcp, size, pdu_tcp);
         coap_pdu_encode_header(pdu_udp, COAP_PROTO_TCP);
         coap_show_pdu(LOG_DEBUG, pdu_tcp);
