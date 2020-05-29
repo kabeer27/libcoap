ff390ce3718d819ef72e19aebc068c5170285f4e
kabeer27 <kabeer.205@gmail.com>
2020-05-29 11:35:44 +0530

    Attempt 3 at improving

--- tests/oss-fuzz/pdu_parse_target.c
+++ tests/oss-fuzz/pdu_parse_target.c
@@ -9,6 +9,7 @@ LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
         coap_pdu_parse(COAP_PROTO_UDP, data, size, pdu);
         coap_pdu_encode_header(pdu, COAP_PROTO_UDP);
         coap_get_query(pdu);
+        coap_get_uri_path(pdu);
         
         coap_show_pdu(LOG_DEBUG, pdu);
         coap_delete_pdu(pdu);
