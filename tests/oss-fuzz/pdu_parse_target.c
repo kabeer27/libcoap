33b77ae4346c04104621c5f29712daf99e2cdcba
kabeer27 <kabeer.205@gmail.com>
2020-05-29 12:09:22 +0530

    Minor pointer fix

--- tests/oss-fuzz/pdu_parse_target.c
+++ tests/oss-fuzz/pdu_parse_target.c
@@ -8,9 +8,9 @@ LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
         coap_set_log_level(LOG_DEBUG);
         coap_pdu_parse(COAP_PROTO_UDP, data, size, pdu);
         coap_pdu_encode_header(pdu, COAP_PROTO_UDP);
-        coap_string_t query = coap_get_query(pdu);
-        coap_string_t uri_path = coap_get_uri_path(pdu);
-        
+        coap_string_t *query = coap_get_query(pdu);
+        coap_string_t *uri_path = coap_get_uri_path(pdu);
+
         coap_delete_string(query);
         coap_delete_string(uri_path);
         coap_show_pdu(LOG_DEBUG, pdu);
