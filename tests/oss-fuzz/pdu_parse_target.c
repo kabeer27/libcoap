09e94546847bfb7c3bf2f2be1bd6632c8e8d5fbc
kabeer27 <kabeer.205@gmail.com>
2020-05-29 10:52:11 +0530

    Attempt 2 improving fuzzer

--- tests/oss-fuzz/pdu_parse_target.c
+++ tests/oss-fuzz/pdu_parse_target.c
@@ -2,24 +2,14 @@
 
 int
 LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-    const uint8_t *data_tcp = data;
-    
-    coap_pdu_t *pdu_udp = coap_pdu_init(0, 0, 0, size);
-    if (pdu_udp) {
-        coap_set_log_level(LOG_DEBUG);
-        coap_pdu_parse(COAP_PROTO_UDP, data, size, pdu_udp);
-        coap_pdu_encode_header(pdu_udp, COAP_PROTO_UDP);
-        coap_show_pdu(LOG_DEBUG, pdu_udp);
-        coap_delete_pdu(pdu_udp);
-    }
 
-    coap_pdu_t *pdu_tcp = coap_pdu_init(0, 0, 0, size);
-    if (pdu_tcp) {
+    coap_pdu_t *pdu = coap_pdu_init(0, 0, 0, size);
+    if (pdu) {
         coap_set_log_level(LOG_DEBUG);
-        coap_pdu_parse(COAP_PROTO_TCP, data_tcp, size, pdu_tcp);
-        coap_pdu_encode_header(pdu_udp, COAP_PROTO_TCP);
-        coap_show_pdu(LOG_DEBUG, pdu_tcp);
-        coap_delete_pdu(pdu_tcp);
+        coap_pdu_parse(COAP_PROTO_UDP, data, size, pdu);
+        coap_pdu_encode_header(pdu, COAP_PROTO_UDP);
+        coap_show_pdu(LOG_DEBUG, pdu);
+        coap_delete_pdu(pdu);
     }
     return 0;
 }
