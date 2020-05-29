46dea7a39562092fea91e1986b8a5fa61f6ea8db
kabeer27 <kabeer.205@gmail.com>
2020-05-29 12:37:34 +0530

    Minor fix

--- tests/oss-fuzz/split_uri_target.c
+++ tests/oss-fuzz/split_uri_target.c
@@ -2,8 +2,8 @@
 
 int
 LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-    coap_uri_t *uri;
-    coap_split_uri(data, size, uri);
-    coap_clone_uri(uri);
+    coap_uri_t uri;
+    coap_split_uri(data, size, &uri);
+    coap_clone_uri(&uri);
     return 0;
 }
